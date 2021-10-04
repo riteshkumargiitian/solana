0d3e591c6b5a8fff3010655484bbeb3af5cc240b
Naveenmishra1197 <76965435+Naveenmishra1197@users.noreply.github.com>
2021-10-04 15:27:41 +0530

    Update before_install.sh

--- docs/.travis/before_install.sh
+++ docs/.travis/before_install.sh
@@ -5,7 +5,7 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install -y nodejs
 npm install --global docusaurus-init
 # docusaurus-init
-npm docusaurus-init
+npx docusaurus-init
 npm install --global vercel
 # npm install --frozen-lockfile
 
