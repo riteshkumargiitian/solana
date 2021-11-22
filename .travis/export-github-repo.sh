ca4ad7bf2fa06ab6dc75582e24f2705098ff0a21
joeaba <77398477+joeaba@users.noreply.github.com>
2021-11-22 16:39:17 +0530

    Update export-github-repo.sh

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -20,15 +20,25 @@ declare repo_name=$2
   echo "Error: subdir not specified"
   exit 1
 }
+echo "$subdir"
 [[ -n "$repo_name" ]] || {
   echo "Error: repo_name not specified"
   exit 1
 }
+echo "$repo_name"
 
 echo "Exporting $subdir"
 
 set -x
 rm -rf .github_export/"$repo_name"
-git clone https://"$GITHUB_TOKEN "@github.com/solana-labs/"$repo_name" .github_export/"$repo_name"
+git clone https://"$GITHUB_TOKEN"@github.com/joeaba/"$repo_name" .github_export/"$repo_name"
 git filter-repo --subdirectory-filter "$subdir" --target .github_export/"$repo_name"
-git -C .github_export/"$repo_name" push --force https://"$GITHUB_TOKEN "@github.com/solana-labs/"$repo_name"
+# git config --global user.email "lerioaxl@gmail.com"
+# git config --global user.name "joeaba"
+git fetch
+git pull origin master
+git merge
+git checkout master
+git remote -v 
+git fetch
+git -C .github_export/"$repo_name" push --force https://"$GITHUB_TOKEN"@github.com/joeaba/"$repo_name"
