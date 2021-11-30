828af9716c3813ad334b4c4303f3cafd80c26a66
Naveenmishra1197 <76965435+Naveenmishra1197@users.noreply.github.com>
2021-11-30 18:52:25 +0530

    Update export-github-repo.sh

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -56,5 +56,5 @@ set -x
  git remote add origin https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/solana-web3.js.git
  git config pull.rebase false
  git pull origin master --allow-unrelated-histories
- git push
+ git push --set-upstream origin master
 #  git push https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/solana-web3.js.git
