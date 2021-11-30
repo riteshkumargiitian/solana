8d37ef262d9c074d1a33254dbbc9cf9bd213850d
Naveenmishra1197 <76965435+Naveenmishra1197@users.noreply.github.com>
2021-11-30 12:27:40 +0530

    Update export-github-repo.sh

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -31,7 +31,4 @@ set -x
 rm -rf .github_export/"$repo_name"
 git clone https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/"$repo_name" .github_export/"$repo_name"
 git filter-repo --subdirectory-filter "$subdir" --target .github_export/"$repo_name"
-git pull
-git fetch 
-git merge FETCH_HEAD
 git -C .github_export/"$repo_name" push  https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/"$repo_name"
