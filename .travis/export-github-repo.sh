51bfdf6e24852a6c1728886ce54b9c996870c915
Naveenmishra1197 <76965435+Naveenmishra1197@users.noreply.github.com>
2021-11-30 13:42:06 +0530

    adding command git pull git fetch  git merge

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -31,4 +31,7 @@ set -x
 rm -rf .github_export/"$repo_name"
 git clone https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/"$repo_name" .github_export/"$repo_name"
 git filter-repo --subdirectory-filter "$subdir" --target .github_export/"$repo_name"
+git pull
+git fetch 
+git merge
 git -C .github_export/"$repo_name" push  https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/"$repo_name"
