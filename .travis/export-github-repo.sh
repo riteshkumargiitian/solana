22e3f956ad0f59d3aa5df4b2dcfaaf27180d1646
Naveenmishra1197 <76965435+Naveenmishra1197@users.noreply.github.com>
2021-12-01 19:05:05 +0530

    adding the commND

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -50,7 +50,7 @@ set -x
 
 rm -rf .github_export/"$repo_name"
 git clone https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/"$repo_name" .github_export/"$repo_name"
-git filter-repo --subdirectory-filter "$subdir" --target .github_export/"$repo_name"
+git filter-repo --to-subdirectory-filter "$subdir" --target .github_export/"$repo_name"
 git -C .github_export/"$repo_name" push  https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/"$repo_name"
  
 
