b4225843b19bd9c5aec823aa85bbde4a50132142
Naveenmishra1197 <76965435+Naveenmishra1197@users.noreply.github.com>
2021-12-01 12:51:56 +0530

    Update export-github-repo.sh

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -55,8 +55,8 @@ set -x
  cd .github_export/"$repo_name"
  git filter-branch --subdirectory-filter "$subdir" -- --all
  git remote rm origin
-#  git config --global user.email "naveenmishra1197@gmail.com"
-#  git config --global user.name "Naveenmishra1197"
+ git config --global user.email "naveenmishra1197@gmail.com"
+ git config --global user.name "Naveenmishra1197"
  git remote add origin https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/solana-web3.js.git
  git config pull.rebase false
  git pull origin master --allow-unrelated-histories
