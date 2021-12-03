f97ba45969209b34a62b681d7dae5c51237bdc99
Naveenmishra1197 <76965435+Naveenmishra1197@users.noreply.github.com>
2021-12-03 13:42:06 +0530

    Update export-github-repo.sh

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -80,6 +80,7 @@ set -x
 #  git remote add origin https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/"$repo_name"
 #  git config pull.rebase false
  git branch --set-upstream-to origin/master
+ git fetch
  git pull 
  git push https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/"$repo_name"
 
