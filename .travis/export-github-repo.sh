d7ce44846d2c40e2dce71789735ca7c9d9998085
Avinash Rai <33398974+Avnshrai@users.noreply.github.com>
2021-12-03 18:08:41 +0530

    Update export-github-repo.sh

--- .travis/export-github-repo.sh
+++ .travis/export-github-repo.sh
@@ -28,7 +28,21 @@ declare repo_name=$2
 echo "Exporting $subdir"
 
 set -x
-rm -rf .github_export/"$repo_name"
-git clone https://"$GITHUB_TOKEN"@github.com/solana-labs/"$repo_name" .github_export/"$repo_name"
-git filter-repo --subdirectory-filter "$subdir" --target .github_export/"$repo_name"
-git -C .github_export/"$repo_name" push https://"$GITHUB_TOKEN"@github.com/solana-labs/"$repo_name"
+# rm -rf .github_export/"$repo_name"
+# git clone https://"$GITHUB_TOKEN"@github.com/solana-labs/"$repo_name" .github_export/"$repo_name"
+# git filter-repo --subdirectory-filter "$subdir" --target .github_export/"$repo_name"
+# git -C .github_export/"$repo_name" push https://"$GITHUB_TOKEN"@github.com/solana-labs/"$repo_name"
+
+ rm -rf .github_export/"$repo_name"
+ git clone https://github.com/Naveenmishra1197/solana.git .github_export/"$repo_name"
+ cd .github_export/"$repo_name"
+#  git filter-branch --subdirectory-filter "$subdir" -- --all
+ git filter-repo --subdirectory-filter "$subdir" 
+#  git remote rm origin
+ git config --global user.email "naveenmishra1197@gmail.com"
+ git config --global user.name "Naveenmishra1197"
+ git remote add origin https://"$GITHUB_TOKEN"@github.com/Naveenmishra1197/"$repo_name"
+ git config pull.rebase false
+ git pull origin master --allow-unrelated-histories
+ git push --set-upstream origin master
+
