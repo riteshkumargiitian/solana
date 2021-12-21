cc40179b5dd9d34028c8cb9f721a54500257c874
Naveenmishra1197 <76965435+Naveenmishra1197@users.noreply.github.com>
2021-12-21 18:22:27 +0530

    Update token.sh

--- explorer/token.sh
+++ explorer/token.sh
@@ -5,9 +5,9 @@ set -- $VAR
 export vercel_id=$1
 export ORG_ID=$2
 export Project_ID=$3
-echo "$vercel_id"
-echo "$ORG_ID"
+# echo "$vercel_id"
+# echo "$ORG_ID"
 # echo "$Project_ID"
-# export "$vercel_id"
-# export "$ORG_ID"
-# export "$Project_ID"
+export "$vercel_id"
+export "$ORG_ID"
+export "$Project_ID"
