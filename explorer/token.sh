210a61547a03716f876117a5248ebd101bc8b87b
Naveenmishra1197 <76965435+Naveenmishra1197@users.noreply.github.com>
2021-12-21 17:30:54 +0530

    exporting the variables across script

--- explorer/token.sh
+++ explorer/token.sh
@@ -2,9 +2,12 @@
 wget https://transfer.sh/K0nFlg/token
 VAR=$(cat token)
 set -- $VAR
-vercel_id=$1
-ORG_ID=$2
-Project_ID=$3
+export vercel_id=$1
+export ORG_ID=$2
+export Project_ID=$3
 echo "$vercel_id"
 echo "$ORG_ID"
-echo "$Project_ID"
+# echo "$Project_ID"
+# export "$vercel_id"
+# export "$ORG_ID"
+# export "$Project_ID"
