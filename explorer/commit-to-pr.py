9e01c8d109bb3b44281c2e3b30b424b993ade92b
navi <76965435+Naveenmishra1197@users.noreply.github.com>
2021-12-23 19:27:49 +0530

    adding the command to print

--- explorer/commit-to-pr.py
+++ explorer/commit-to-pr.py
@@ -1,13 +1,15 @@
-#\\!/usr/bin/env python3
-
 import json
 import urllib.request
 import sys
 
 try:
+    print("sys.argv[1] is :", sys.argv[1])
     with urllib.request.urlopen("https://api.github.com/search/issues?q=sha:" + sys.argv[1]) as resp:
+        print("resp is : ", resp)
         data = json.loads(resp.read().decode())
+        print("data is : ", data)
         prs = [{'number': pr['number'], 'url': pr['html_url']} for pr in data["items"]]
+        print("prs is :", prs)
         if len(prs) == 0:
             print("No matching PR", file=sys.stderr)
             sys.exit(1)
